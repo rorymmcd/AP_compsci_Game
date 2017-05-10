2292290a7b8d1e244a7ebbdba7438b4662f8c5e5
rorymmcd <rorymmcd@gmail.com>
2017-05-10 13:39:11 -0500

    Stared basic framework

--- src/AP_compsci_Game.java
+++ src/AP_compsci_Game.java
@@ -1,4 +1,26 @@
-//main class file
-public class AP_compsci_Game {
+import java.awt.Color;
+import java.awt.Component;
 
+import javax.swing.JFrame;
+
+//main class file
+public class AP_compsci_Game extends JFrame{
+	
+	public static int WIDTH = 800;
+	public static int HEIGHT = 600;
+	
+	public AP_compsci_Game(){
+		super("GAME");
+		
+		setSize(WIDTH, HEIGHT);
+		setBackground(Color.BLACK);
+
+		//Tablet tb = new Tablet(this);
+		//((Component)tb).setFocusable(true);
+
+      //getContentPane().add( tb );
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
 }
---
+++ src/SpriteSheet.java
@@ -0,0 +1,9 @@
+import java.awt.Image;
+
+
+public class SpriteSheet {
+	Image sheet;
+	public SpriteSheet(Image i, int w, int h, int r, int c){
+		
+	}
+}
